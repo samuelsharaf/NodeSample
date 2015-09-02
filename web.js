f56afe95ccbf57cab0343037ae28efcb695abbae
Samuel Sharaf <samuel.sharaf@salesforce.com>
2015-09-01 20:39:09 -0700

    fixed the app.listen

--- web.js
+++ web.js
@@ -24,7 +24,6 @@ app.use(function(err, req, res, next){
 });
 
 app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'), function(){
-  console.log( 'Express started on http://localhost:' +
-    app.get('port'));
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
